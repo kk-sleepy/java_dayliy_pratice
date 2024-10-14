ec702ba1e7c134d91a58c73755dc97bec9fc0fde
kk-sleepy <1610896646@qq.com>
2024-10-14 11:12:27 +0800

    每日一题，动态规划、找数学规律

---
+++ src/main/java/dayliy_test/oct/t1013.java
@@ -0,0 +1,25 @@
+package dayliy_test.oct;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+class Solution {
+    public int twoEggDrop(int n) {
+        int f[] = new int[n+1];
+        Arrays.fill(f, Integer.MAX_VALUE);
+        f[0] = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                f[i] = Math.min(f[i], Math.max(j-1, f[i-j])+1);
+            }
+        }
+        return f[n];
+    }
+
+}
+public class t1013 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.twoEggDrop(100));
+    }
+}
