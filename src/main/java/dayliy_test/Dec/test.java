e97322a5ed724b34e3abdd41fdf7393e3bde85c8
kk-sleepy <1610896646@qq.com>
2025-03-04 15:06:18 +0800

    每日一题，3月4号，切割回文串

---
+++ src/main/java/dayliy_test/Dec/test.java
@@ -0,0 +1,12 @@
+package dayliy_test.Dec;
+
+public class test {
+    private String name;
+    public String findString(String str){
+        return str;
+    }
+    public static void main(String[] arg){
+        test t = new test();
+        System.out.println(t.findString("kk"));
+    }
+}
