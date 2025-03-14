1f85c905fb0feb85e5a0282f09c39f9c922f762e
kk-sleepy <1610896646@qq.com>
2025-03-14 20:34:52 +0800

    3月14号，哈希表

---
+++ src/main/java/daliyTest2025/May/twoSum1/Solution.java
@@ -0,0 +1,27 @@
+package daliyTest2025.May.twoSum1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int[] twoSum(int[] nums,int target){
+        Map<Integer,Integer> ans = new HashMap<>();
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            int x = ans.getOrDefault(target-nums[i],-1);
+            if(x!=-1){
+                return new int[] {x,i};
+            }
+            ans.put(nums[i],i);
+        }
+        return new int[0];
+    }
+    public static void main(String[] args){
+        Solution s = new Solution();
+        int[] sums = {3,2,4};
+        int[] data = s.twoSum(sums,6);
+        for(int x:data){
+            System.out.println(x);
+        }
+    }
+}
