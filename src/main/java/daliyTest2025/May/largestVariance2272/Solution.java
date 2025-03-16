7ed95dd80aa946af71f570313de66ab05a25d734
kk-sleepy <1610896646@qq.com>
2025-03-16 15:20:20 +0800

    3月16号，dp解决数组内最大波动值问题,优化后

--- src/main/java/daliyTest2025/May/largestVariance2272/Solution.java
+++ src/main/java/daliyTest2025/May/largestVariance2272/Solution.java
@@ -4,24 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
-    public int largestVariance(String s){
+    public int largestVariance(String s) {
         int ans = 0;
-        int n = s.length();
-        for(char a='a';a<='z';a++){
-            for(char b='a';b<='z';b++){
-                if(a==b)
+        int[][] f0 = new int[26][26];
+        int[][] f1 = new int[26][26];
+        for (int[] row : f1) {
+            Arrays.fill(row, Integer.MIN_VALUE);
+        }
+
+        for (char ch : s.toCharArray()) {
+            ch -= 'a';
+            // 遍历到 ch 时，只需计算 a=ch 或者 b=ch 的状态，其他状态和 ch 无关，f 值不变
+            for (int i = 0; i < 26; i++) {
+                if (i == ch) {
                     continue;
-                int f0=0, f1=Integer.MIN_VALUE;
-                for(int i=0;i<n;i++){
-                    char c = s.charAt(i);
-                    if(c==a){
-                        f1 += 1;
-                        f0 = Math.max(f0,0) + 1;
-                    }else if(c==b){
-                        f1 = f0 = Math.max(f0,0) - 1;
-                    }
-                    ans = Math.max(f1,ans);
                 }
+                // 假设出现次数最多的字母 a=ch，更新所有 b=i 的状态
+                f0[ch][i] = Math.max(f0[ch][i], 0) + 1;
+                f1[ch][i]++;
+                // 假设出现次数最少的字母 b=ch，更新所有 a=i 的状态
+                f1[i][ch] = f0[i][ch] = Math.max(f0[i][ch], 0) - 1;
+                ans = Math.max(ans, Math.max(f1[ch][i], f1[i][ch]));
             }
         }
         return ans;
