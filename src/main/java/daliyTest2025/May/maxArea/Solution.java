4bba1440fad062916bcba625c37a1a524ce1d13c
kk-sleepy <1610896646@qq.com>
2025-03-22 18:15:24 +0800

    3月22号，双指针+贪心，解决 底*高 = 最大面积问题

---
+++ src/main/java/daliyTest2025/May/maxArea/Solution.java
@@ -0,0 +1,26 @@
+package daliyTest2025.May.maxArea;
+
+public class Solution {
+    public int maxArea(int[] height) {
+        int ans = 0;
+        int l = 0, r = height.length - 1;
+        while (l < r) {
+            int h;
+            if(height[l]>=height[r]){
+                h = height[r];
+                ans = Math.max(ans, h * (r - l));
+                r -= 1;
+            }else{
+                h = height[l];
+                ans = Math.max(ans, h * (r - l));
+                l += 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+    }
+}
