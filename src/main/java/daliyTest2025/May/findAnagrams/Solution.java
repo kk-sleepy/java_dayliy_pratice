ba0fe51c9381d87b379cdcd3c425511713ac5516
kk-sleepy <1610896646@qq.com>
2025-03-25 15:28:39 +0800

    3月25号，哈希表 + 滑动窗口解决字符串异位词

---
+++ src/main/java/daliyTest2025/May/findAnagrams/Solution.java
@@ -0,0 +1,42 @@
+package daliyTest2025.May.findAnagrams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p){
+        List<Integer> ans = new ArrayList<>();
+        int slen = s.length(), plen = p.length();
+        int[] ss = new int[26];
+        int[] pp = new int[26];
+        for(int i=0;i<plen;i++){
+            int c = p.charAt(i)-'a';
+            pp[c] += 1;
+            ss[s.charAt(i)-'a'] += 1;
+        }
+        for(int i =plen;i<slen;i++){
+            if(check(ss,pp))
+                ans.add(i-plen);
+            ss[s.charAt(i-plen)-'a'] -= 1;
+            ss[s.charAt(i)-'a'] += 1;
+        }
+        if(check(ss,pp))
+            ans.add(slen-plen);
+        return ans;
+    }
+    private boolean check(int[] s, int[] p){
+        for(int i=0;i<26;i++){
+            if(s[i]!=p[i])
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        List<Integer> ans = s.findAnagrams("abab","ab");
+        for(Integer data : ans){
+            System.out.println(data);
+        }
+    }
+}
