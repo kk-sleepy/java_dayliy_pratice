9ac8848975ede7f77528968c65971ce00bd55021
kk-sleepy <1610896646@qq.com>
2025-05-25 20:20:26 +0800

    5月25号，dp类爬楼梯

---
+++ src/main/java/daliyTest2025/May/combinationSum4/Solution.java
@@ -0,0 +1,31 @@
+package daliyTest2025.May.combinationSum4;
+
+import java.util.Arrays;
+
+class Solution {
+    int[] memo;
+    public int combinationSum4(int[] nums, int target) {
+        int n = nums.length;
+        memo = new int[target+1];
+        Arrays.fill(memo,-1);
+        return dfs(nums,target);
+    }
+    int dfs(int[] nums,int j){
+        if(j == 0)
+            return 1;
+        if(memo[j] != -1)
+            return memo[j];
+        int res = 0;
+        for(int x : nums){
+            if(j>=x){
+                res += dfs(nums,j-x);
+            }
+        }
+        return memo[j] = res;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.combinationSum4(new int[]{1,2,3},4));
+    }
+}
