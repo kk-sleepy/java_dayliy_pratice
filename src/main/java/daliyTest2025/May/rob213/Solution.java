83bfd69930793382013df1cd5cd1f22eea82eb51
kk-sleepy <1610896646@qq.com>
2025-05-26 16:31:50 +0800

    5月25号，dp打家劫舍问题

--- src/main/java/daliyTest2025/May/rob213/Solution.java
+++ src/main/java/daliyTest2025/May/rob213/Solution.java
@@ -1,23 +1,25 @@
-package daliyTest2025.May;
+package daliyTest2025.May.rob213;
 
 import java.util.Arrays;
 
 class Solution {
     public int rob(int[] nums) {
-        return dfs(0,nums.length,nums);
+        int n = nums.length;
+        if(n==2){
+            return Math.max(nums[0],nums[1]);
+        }
+        return Math.max(nums[0] + dfs(2,n-1,nums),dfs(1,n,nums));
     }
     int dfs(int i,int j,int[] nums){
+        if(j<=i)
+            return 0;
         int n = nums.length;
-        int[] dp = new int[n];
-        dp[i] = nums[0];
+        int[] dp = new int[n+1];
         dp[i+1] = Math.max(nums[i],nums[i+1]);
+        dp[i] = nums[i];
         for(int k = i+2;k<j;k++){
             dp[k] = Math.max(dp[k-1],dp[k-2]+nums[k]);
         }
         return dp[j-1];
-    }
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        System.out.println(s.rob(new int[]{1,2,3,1}));;
     }
 }
